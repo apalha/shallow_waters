3a76a51bb914be358b2389832115f643fceebb67
Artur Palha <artur.palha@gmail.com>
2024-07-20 21:48:01 +0200

    Updates solver - Solves using nonlinear solver provided by Firedrake to make the whole notebook simpler. - Generates the weak form outside the for loop. - Adds the full implementation from scratch of the Newton-Raphson solver on a separate file.

--- shallow_waters_newton_explicit.py
+++ shallow_waters_newton_explicit.py
@@ -48,7 +48,6 @@ sol_outfile = firedrake.VTKFile(results_path + "shallow_waters.pvd")  # file nam
 # Select the test to run
 #   - Gaussian hill initial condition: `gaussian_hill`
 #   - Travelling wave: `travelling_wave`
-#   - Double vortex: `double_vortex`
 #
 
 # %%
@@ -91,7 +90,7 @@ use_quad_complex = True  # options:
 #    - `poisson`: uses the Poisson integrator, which is exactly energy conserving.
 
 # %%
-time_integrator = "midpoint"  # options:
+time_integrator = "midpoint"   # options:
                               #    "midpoint"
                               #    "poisson" 
 dt = test_case_default_parameters["dt"]  # time step size
@@ -111,12 +110,15 @@ newton_max_iter = 20  # maximum number of iterations to perform per Newton-Raphs
 #
 
 # %%
-MUMPS = {"snes_type": "newtonls",
-         "ksp_type" : "preonly",
-         "pc_type" : "lu",
-         "pc_factor_mat_solver_type": "mumps",
-         "ksp_rtol": newton_tol,
-         "ksp_max_it": newton_max_iter}
+MUMPS = {
+        "snes_monitor": None,
+        "snes_max_it":1000,
+        "ksp_type": "preonly",
+        "mat_type": "aij",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps"
+        }
+
 
 # %% [markdown]
 # ## Initial conditions
@@ -308,11 +310,11 @@ K_1.rename("Kinetic energy","Kinetic energy")
 #
 
 # %% [markdown]
-# ### Define the midpoint integrator one-step evolution nonlinear weak form
+# ### Define the midpoint integrator one-step evolution function
 #
 
 # %%
-def weak_form_mid_point(u_0, h_0, q_0, K_0, F_0, G, D, S, dt):
+def advance_mid_point_newton(u_0, u_1, h_0, h_1, q_0, q_1, K_0, K_1, F_0, F_1, G, D, S, dt):
     # Note that here the unknowns are:
     #   u^{k+1}, h^{k+1}, q^{k+1} --> All at the next time step as for the midpoint rule
     #   F^{k+1/2}, K^{k + 1/2}  --> These two are computed at the midpoint
@@ -321,34 +323,121 @@ def weak_form_mid_point(u_0, h_0, q_0, K_0, F_0, G, D, S, dt):
 
     # Setup the mixed space and the trial and test functions
     M = D * S * G * S * D  # (u, h, q, K, F)
+    mixed_u_trial, mixed_h_trial, mixed_q_trial, mixed_K_trial, mixed_F_trial = firedrake.TrialFunctions(M)
     mixed_u_test, mixed_h_test, mixed_q_test, mixed_K_test, mixed_F_test = firedrake.TestFunctions(M)
 
     # Define the update solution for the Newton-Rapshon iterative solve
-    sol = firedrake.Function(M)  # Newton correction
-    u_sol, h_sol, q_sol, K_sol, F_sol = firedrake.split(sol)
+    dsol = firedrake.Function(M)  # Newton correction
+    du, dh, dq, dK, dF = dsol.subfunctions
 
-    # Nonlinear weak form
-    F = firedrake.inner(u_sol - u_0, mixed_u_test)*firedrake.dx + \
-        dt*firedrake.inner(cross_scalar(q_sol, F_sol), mixed_u_test)*firedrake.dx - \
-        dt*firedrake.inner(K_sol, firedrake.div(mixed_u_test))*firedrake.dx - \
-        0.5*g*dt*firedrake.inner(h_0 + h_sol, firedrake.div(mixed_u_test))*firedrake.dx + \
-        firedrake.inner(h_sol - h_0, mixed_h_test)*firedrake.dx + \
-        dt*firedrake.inner(firedrake.div(F_sol), mixed_h_test)*firedrake.dx + \
-        firedrake.inner(0.5*(h_sol + h_0)*q_sol, mixed_q_test)*firedrake.dx - \
-        firedrake.inner(0.5*(u_sol + u_0), rot(mixed_q_test))*firedrake.dx + \
-        firedrake.inner(K_sol, mixed_K_test)*firedrake.dx - \
-        0.5*firedrake.inner(0.5*(u_sol + u_0), 0.5*(u_sol + u_0)*mixed_K_test)*firedrake.dx + \
-        firedrake.inner(F_sol, mixed_F_test)*firedrake.dx - \
-        firedrake.inner(0.5*(h_sol*u_sol + h_0*u_0), mixed_F_test)*firedrake.dx
-    
-    return sol, F
+    # Define temporary solution for updating the solution during Newton-Raphson method (this is very much Firedrake dependent)
+    sol_temp = firedrake.Function(M)  # temporary next time step for fixed-point error computation
+    u_temp, h_temp, q_temp, K_temp, F_temp = sol_temp.subfunctions
+
+    # Initialyze Newton-Raphson iteration
+    newton_iter = 1
+    newton_error = 1.0
+
+    # Print columns labels of Newton-Raphson output information
+    print("                           u         |           h         |           q         |           K         |           F         |       Total ")
+        
+    while (newton_error > newton_tol) and (newton_iter < newton_max_iter):
+        # Setup Newton method
+
+        # Right hand side
+        F = firedrake.inner(u_1 - u_0, mixed_u_test)*firedrake.dx + \
+            dt*firedrake.inner(cross_scalar(q_1, F_1), mixed_u_test)*firedrake.dx - \
+            dt*firedrake.inner(K_1, firedrake.div(mixed_u_test))*firedrake.dx - \
+            0.5*g*dt*firedrake.inner(h_0 + h_1, firedrake.div(mixed_u_test))*firedrake.dx + \
+            firedrake.inner(h_1 - h_0, mixed_h_test)*firedrake.dx + \
+            dt*firedrake.inner(firedrake.div(F_1), mixed_h_test)*firedrake.dx + \
+            firedrake.inner(0.5*(h_1+ h_0)*q_1, mixed_q_test)*firedrake.dx - \
+            firedrake.inner(0.5*(u_1 + u_0), rot(mixed_q_test))*firedrake.dx + \
+            firedrake.inner(K_1, mixed_K_test)*firedrake.dx - \
+            0.5*firedrake.inner(0.5*(u_1 + u_0), 0.5*(u_1 + u_0)*mixed_K_test)*firedrake.dx + \
+            firedrake.inner(F_1, mixed_F_test)*firedrake.dx - \
+            firedrake.inner(0.5*(h_1*u_1 + h_0*u_0), mixed_F_test)*firedrake.dx
+        
+        # Jacobian
+        a = firedrake.inner(mixed_u_trial, mixed_u_test)*firedrake.dx + \
+            dt*firedrake.inner(cross_scalar(mixed_q_trial, F_1), mixed_u_test)*firedrake.dx + \
+            dt*firedrake.inner(cross_scalar(q_1, mixed_F_trial), mixed_u_test)*firedrake.dx - \
+            dt*firedrake.inner(mixed_K_trial, firedrake.div(mixed_u_test))*firedrake.dx - \
+            0.5*g*dt*firedrake.inner(mixed_h_trial, firedrake.div(mixed_u_test))*firedrake.dx + \
+            firedrake.inner(mixed_h_trial, mixed_h_test)*firedrake.dx + \
+            dt*firedrake.inner(firedrake.div(mixed_F_trial), mixed_h_test)*firedrake.dx + \
+            0.5*firedrake.inner(mixed_h_trial*q_1, mixed_q_test)*firedrake.dx + \
+            0.5*firedrake.inner((h_1 + h_0)*mixed_q_trial, mixed_q_test)*firedrake.dx - \
+            0.5*firedrake.inner(mixed_u_trial, rot(mixed_q_test))*firedrake.dx + \
+            firedrake.inner(mixed_K_trial, mixed_K_test)*firedrake.dx - \
+            firedrake.inner(0.5*mixed_u_trial, 0.5*(u_1 + u_0)*mixed_K_test)*firedrake.dx + \
+            firedrake.inner(mixed_F_trial, mixed_F_test)*firedrake.dx - \
+            0.5*firedrake.inner(mixed_h_trial*u_1, mixed_F_test)*firedrake.dx - \
+            0.5*firedrake.inner(h_1*mixed_u_trial, mixed_F_test)*firedrake.dx
+
+        # Solve fixed point update step
+        A = firedrake.assemble(a)
+        b = firedrake.assemble(F)
+
+        firedrake.solve(A, dsol.vector(), b, solver_parameters=MUMPS)
+
+        # Compute Newton error 
+        norm_u = firedrake.norm(u_1)
+        if norm_u < 1.0:
+            newton_error_u = firedrake.norm(du)
+        else:
+            newton_error_u = firedrake.norm(du) / norm_u
+
+        norm_h = firedrake.norm(h_1)   
+        if norm_h < 1.0:
+            newton_error_h = firedrake.norm(dh)
+        else:
+            newton_error_h = firedrake.norm(dh)/norm_h
+        
+        norm_q = firedrake.norm(q_1)   
+        if norm_q < 1.0:
+            newton_error_q = firedrake.norm(dq)
+        else:
+            newton_error_q = firedrake.norm(dq)/norm_q
+        
+        norm_K = firedrake.norm(K_1)   
+        if norm_K < 1.0:
+            newton_error_K = firedrake.norm(dK)
+        else:
+            newton_error_K = firedrake.norm(dK)/norm_K
+
+        norm_F = firedrake.norm(F_1)   
+        if norm_F < 1.0:
+            newton_error_F = firedrake.norm(dF)
+        else:
+            newton_error_F = firedrake.norm(dF)/norm_F
+
+        newton_error = (newton_error_u + newton_error_h + newton_error_q + newton_error_K + newton_error_F)/5.0
+
+        print("   Newton error: {newton_error_u:19.16f} | {newton_error_h:19.16f} | {newton_error_q:19.16f} | "\
+                "{newton_error_K:19.16f} | {newton_error_F:19.16f} | {newton_error:19.16f}".format(newton_error_u=newton_error_u, \
+                newton_error_h=newton_error_h, newton_error_q=newton_error_q, newton_error_K=newton_error_K, \
+                newton_error_F=newton_error_F, newton_error=newton_error))
+        
+        # Update solution
+        u_temp.vector()[:] = u_1.vector()[:] - du.vector()[:]
+        h_temp.vector()[:] = h_1.vector()[:] - dh.vector()[:]
+        q_temp.vector()[:] = q_1.vector()[:] - dq.vector()[:]
+        K_temp.vector()[:] = K_1.vector()[:] - dK.vector()[:]
+        F_temp.vector()[:] = F_1.vector()[:] - dF.vector()[:]
+
+        u_1.assign(u_temp)
+        h_1.assign(h_temp)
+        q_1.assign(q_temp)
+        K_1.assign(K_temp)
+        F_1.assign(F_temp)
 
 # %% [markdown]
-# ### Define the Poisson integrator one-step evolution nonlinear weak form
+# ### Define the Poisson integrator one-step evolution function
 #
 
 # %%
-def weak_form_poisson(u_0, h_0, q_0, K_0, F_0, G, D, S, dt):
+def advance_poisson_newton(u_0, u_1, h_0, h_1, q_0, q_1, K_0, K_1, F_0, F_1, G, D, S, dt):
     # Note that here the unknowns are:
     #   u^{k+1}, h^{k+1}, q^{k+1} --> All at the next time step as for the midpoint rule
     #   F^{k+1/2}, K^{k + 1/2}  --> These two are computed at the midpoint with a symmetrizing average
@@ -356,31 +445,121 @@ def weak_form_poisson(u_0, h_0, q_0, K_0, F_0, G, D, S, dt):
     #                               F^{k-1/2} = F_0 and K^{k-1/2} = K_0
 
     M = D * S * G * S * D  # (u, h, q, K, F)
+    mixed_u_trial, mixed_h_trial, mixed_q_trial, mixed_K_trial, mixed_F_trial = firedrake.TrialFunctions(M)
     mixed_u_test, mixed_h_test, mixed_q_test, mixed_K_test, mixed_F_test = firedrake.TestFunctions(M)
 
-    sol = firedrake.Function(M)  # Newton correction
-    u_sol, h_sol, q_sol, K_sol, F_sol = firedrake.split(sol)
-        
-    # Weak nonlinear form
-    F = firedrake.inner(u_sol - u_0, mixed_u_test)*firedrake.dx + \
-        0.5*dt*firedrake.inner(cross_scalar(q_0 + q_sol, F_sol), mixed_u_test)*firedrake.dx - \
-        dt*firedrake.inner(K_sol, firedrake.div(mixed_u_test))*firedrake.dx - \
-        0.5*g*dt*firedrake.inner(h_0 + h_sol, firedrake.div(mixed_u_test))*firedrake.dx + \
-        firedrake.inner(h_sol - h_0, mixed_h_test)*firedrake.dx + \
-        dt*firedrake.inner(firedrake.div(F_sol), mixed_h_test)*firedrake.dx + \
-        firedrake.inner(h_sol*q_sol, mixed_q_test)*firedrake.dx - \
-        firedrake.inner(u_sol, rot(mixed_q_test))*firedrake.dx + \
-        firedrake.inner(K_sol, mixed_K_test)*firedrake.dx - \
-        (1.0/6.0)*firedrake.inner(u_sol, u_sol*mixed_K_test)*firedrake.dx - \
-        (1.0/6.0)*firedrake.inner(u_sol, u_0*mixed_K_test)*firedrake.dx - \
-        (1.0/6.0)*firedrake.inner(u_0, u_0*mixed_K_test)*firedrake.dx + \
-        firedrake.inner(F_sol, mixed_F_test)*firedrake.dx - \
-        (1.0/3.0)*firedrake.inner(h_0*u_0, mixed_F_test)*firedrake.dx - \
-        (1.0/6.0)*firedrake.inner(h_0*u_sol, mixed_F_test)*firedrake.dx - \
-        (1.0/6.0)*firedrake.inner(h_sol*u_0, mixed_F_test)*firedrake.dx - \
-        (1.0/3.0)*firedrake.inner(h_sol*u_sol, mixed_F_test)*firedrake.dx
+    dsol = firedrake.Function(M)  # Newton correction
+    du, dh, dq, dK, dF = dsol.subfunctions
 
-    return sol, F
+    sol_temp = firedrake.Function(M)  # temporary next time step for fixed-point error computation
+    u_temp, h_temp, q_temp, K_temp, F_temp = sol_temp.subfunctions
+
+    newton_iter = 1
+    newton_error = 1.0
+
+    # Print columns labels of Newton-Raphson output information
+    print("                           u         |           h         |           q         |           K         |           F         |       Total ")
+        
+    while (newton_error > newton_tol) and (newton_iter < newton_max_iter):
+
+        # Setup Newton method
+        
+        # Right hand side
+        F = firedrake.inner(u_1 - u_0, mixed_u_test)*firedrake.dx + \
+            0.5*dt*firedrake.inner(cross_scalar(q_0 + q_1, F_1), mixed_u_test)*firedrake.dx - \
+            dt*firedrake.inner(K_1, firedrake.div(mixed_u_test))*firedrake.dx - \
+            0.5*g*dt*firedrake.inner(h_0 + h_1, firedrake.div(mixed_u_test))*firedrake.dx + \
+            firedrake.inner(h_1 - h_0, mixed_h_test)*firedrake.dx + \
+            dt*firedrake.inner(firedrake.div(F_1), mixed_h_test)*firedrake.dx + \
+            firedrake.inner(h_1*q_1, mixed_q_test)*firedrake.dx - \
+            firedrake.inner(u_1, rot(mixed_q_test))*firedrake.dx + \
+            firedrake.inner(K_1, mixed_K_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(u_1, u_1*mixed_K_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(u_1, u_0*mixed_K_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(u_0, u_0*mixed_K_test)*firedrake.dx + \
+            firedrake.inner(F_1, mixed_F_test)*firedrake.dx - \
+            (1.0/3.0)*firedrake.inner(h_0*u_0, mixed_F_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(h_0*u_1, mixed_F_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(h_1*u_0, mixed_F_test)*firedrake.dx - \
+            (1.0/3.0)*firedrake.inner(h_1*u_1, mixed_F_test)*firedrake.dx
+        
+        # Jacobian
+        a = firedrake.inner(mixed_u_trial, mixed_u_test)*firedrake.dx + \
+            0.5*dt*firedrake.inner(cross_scalar(mixed_q_trial, F_1), mixed_u_test)*firedrake.dx + \
+            0.5*dt*firedrake.inner(cross_scalar(q_0 + q_1, mixed_F_trial), mixed_u_test)*firedrake.dx - \
+            dt*firedrake.inner(mixed_K_trial, firedrake.div(mixed_u_test))*firedrake.dx - \
+            0.5*g*dt*firedrake.inner(mixed_h_trial, firedrake.div(mixed_u_test))*firedrake.dx + \
+            firedrake.inner(mixed_h_trial, mixed_h_test)*firedrake.dx + \
+            dt*firedrake.inner(firedrake.div(mixed_F_trial), mixed_h_test)*firedrake.dx + \
+            firedrake.inner(mixed_h_trial*q_1, mixed_q_test)*firedrake.dx + \
+            firedrake.inner(h_1*mixed_q_trial, mixed_q_test)*firedrake.dx - \
+            firedrake.inner(mixed_u_trial, rot(mixed_q_test))*firedrake.dx + \
+            firedrake.inner(mixed_F_trial, mixed_F_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(h_1*mixed_u_trial, mixed_F_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(mixed_h_trial*u_1, mixed_F_test)*firedrake.dx - \
+            (1.0/3.0)*firedrake.inner(h_1*mixed_u_trial, mixed_F_test)*firedrake.dx - \
+            (1.0/3.0)*firedrake.inner(mixed_h_trial*u_1, mixed_F_test)*firedrake.dx + \
+            firedrake.inner(mixed_K_trial, mixed_K_test)*firedrake.dx - \
+            (1.0/6.0)*firedrake.inner(mixed_u_trial, u_0*mixed_K_test)*firedrake.dx - \
+            (1.0/3.0)*firedrake.inner(mixed_u_trial, u_1*mixed_K_test)*firedrake.dx
+        
+        # Solve fixed point update step
+        A = firedrake.assemble(a)
+        b = firedrake.assemble(F)
+
+        firedrake.solve(A, dsol.vector(), b, solver_parameters=MUMPS)
+
+        # Compute Newton error 
+        norm_u = firedrake.norm(u_1)
+        if norm_u < 1.0:
+            newton_error_u = firedrake.norm(du)
+        else:
+            newton_error_u = firedrake.norm(du) / norm_u
+
+        norm_h = firedrake.norm(h_1)   
+        if norm_h < 1.0:
+            newton_error_h = firedrake.norm(dh)
+        else:
+            newton_error_h = firedrake.norm(dh)/norm_h
+        
+        norm_q = firedrake.norm(q_1)   
+        if norm_q < 1.0:
+            newton_error_q = firedrake.norm(dq)
+        else:
+            newton_error_q = firedrake.norm(dq)/norm_q
+        
+        norm_K = firedrake.norm(K_1)   
+        if norm_K < 1.0:
+            newton_error_K = firedrake.norm(dK)
+        else:
+            newton_error_K = firedrake.norm(dK)/norm_K
+
+        norm_F = firedrake.norm(F_1)   
+        if norm_F < 1.0:
+            newton_error_F = firedrake.norm(dF)
+        else:
+            newton_error_F = firedrake.norm(dF)/norm_F
+
+        newton_error = (newton_error_u + newton_error_h + newton_error_q + newton_error_K + newton_error_F)/5.0
+
+        print("   Newton error: {newton_error_u:19.16f} | {newton_error_h:19.16f} | {newton_error_q:19.16f} | "\
+                "{newton_error_K:19.16f} | {newton_error_F:19.16f} | {newton_error:19.16f}".format(newton_error_u=newton_error_u, \
+                newton_error_h=newton_error_h, newton_error_q=newton_error_q, newton_error_K=newton_error_K, \
+                newton_error_F=newton_error_F, newton_error=newton_error))
+        
+        # Update solution
+        u_temp.vector()[:] = u_1.vector()[:] - du.vector()[:]
+        h_temp.vector()[:] = h_1.vector()[:] - dh.vector()[:]
+        q_temp.vector()[:] = q_1.vector()[:] - dq.vector()[:]
+        K_temp.vector()[:] = K_1.vector()[:] - dK.vector()[:]
+        F_temp.vector()[:] = F_1.vector()[:] - dF.vector()[:]
+
+        u_1.assign(u_temp)
+        h_1.assign(h_temp)
+        q_1.assign(q_temp)
+        K_1.assign(K_temp)
+        F_1.assign(F_temp)
+
 
 # %% [markdown]
 # ## Start time evolution
@@ -450,17 +629,6 @@ sol_outfile.write(u_1, h_1, q_1, K_1, F_1, time = 0.0)
 # see above.
 
 # %%
-if time_integrator == "midpoint":
-    sol, F = weak_form_mid_point(u_0, h_0, q_0, K_0, F_0, G, D, S, dt)
-elif time_integrator == "poisson":
-    sol, F = weak_form_poisson(u_0, h_0, q_0, K_0, F_0, G, D, S, dt)
-else:
-    raise Exception("Type of time integrator not recognized, you can use: midpoint or poisson")
-
-# Extract the subfields of the mixed solution
-u_sol, h_sol, q_sol, K_sol, F_sol = sol.subfunctions
-
-# %%
 for time_step in range(1, n_t_steps):
     print("\nT step: " + str(time_step))
 
@@ -471,23 +639,14 @@ for time_step in range(1, n_t_steps):
     K_0.assign(K_1)
     F_0.assign(F_1)
 
-    # Reinitialized the solution to the nonlinear problem
-    u_sol.assign(u_1)
-    h_sol.assign(h_1)
-    q_sol.assign(q_1)
-    K_sol.assign(K_1)
-    F_sol.assign(F_1)
-
-    # Step in time (solve the nonlinear weak problem)
-    firedrake.solve(F == 0, sol, solver_parameters=MUMPS)
-
-    # Update the solution
-    u_1.assign(u_sol)
-    h_1.assign(h_sol)
-    q_1.assign(q_sol)
-    K_1.assign(K_sol)
-    F_1.assign(F_sol)
-
+    # Step in time
+    if time_integrator == "midpoint":
+        advance_mid_point_newton(u_0, u_1, h_0, h_1, q_0, q_1, K_0, K_1, F_0, F_1, G, D, S, dt)
+    elif time_integrator == "poisson":
+        advance_poisson_newton(u_0, u_1, h_0, h_1, q_0, q_1, K_0, K_1, F_0, F_1, G, D, S, dt)
+    else:
+        raise Exception("Type of time integrator not recognized, you can use: midpoint or poisson")
+    
     # Export the solutions to paraview 
     sol_outfile.write(u_1, h_1, q_1, K_1, F_1, time = dt*time_step)
 
